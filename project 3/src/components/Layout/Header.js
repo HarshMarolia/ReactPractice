9a9afeb4ec891078cc52dd93fb7b3d540227b621
HarshMarolia <maroliaharsh@gmail.com>
2023-06-17 11:55:54 +0530

    Completed project 3

--- project 3/src/components/Layout/Header.js
+++ project 3/src/components/Layout/Header.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import mealsImage from '../../assets/meals.jpg'
+import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCardButton'
 
 const Header = (props) => {
   return (
     <>
-    <header>
+    <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>cart</button>
+        <HeaderCartButton onClick={props.onShowCart}/>
     </header>
-    <div>
-        <img src="" alt="" />
+    <div className={classes['main-image']}>
+        <img src={mealsImage} alt="A table full of delicious food." />
     </div>
     </>
   )
